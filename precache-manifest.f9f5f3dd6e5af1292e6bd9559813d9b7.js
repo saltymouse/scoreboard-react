64429a02a2e6b4c62781f9700e51cf6cedc0d811
saltymouse <7547649+saltymouse@users.noreply.github.com>
2019-11-25 17:32:16 -0800

    publish: initial commit 🐭
    generated from commit ad36e0c562f821c74fbf6a8e645f8790faa37121

---
+++ precache-manifest.f9f5f3dd6e5af1292e6bd9559813d9b7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa0532ec505d2f850e1e222b8d0d71c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "719dc398496110f424bf",
+    "url": "/static/css/main.37a683ff.chunk.css"
+  },
+  {
+    "revision": "d939b44612a39397fb02",
+    "url": "/static/js/2.ee1ab196.chunk.js"
+  },
+  {
+    "revision": "719dc398496110f424bf",
+    "url": "/static/js/main.c5e82562.chunk.js"
+  },
+  {
+    "revision": "cbe1c11ca9ddf6e17766",
+    "url": "/static/js/runtime-main.634ad7a2.js"
+  },
+  {
+    "revision": "10a5b071548379871a331ddae24e289f",
+    "url": "/static/media/check.10a5b071.svg"
+  },
+  {
+    "revision": "fcd33b5c27a7f23a50e96dce05e8b58d",
+    "url": "/static/media/minus.fcd33b5c.svg"
+  },
+  {
+    "revision": "7c6c8212afc5044e4b60f59552e57e69",
+    "url": "/static/media/plus.7c6c8212.svg"
+  },
+  {
+    "revision": "7fad484d3fc1b16b62081c4dfa80d5d9",
+    "url": "/static/media/user-minus.7fad484d.svg"
+  },
+  {
+    "revision": "5047d00469e004c51f6b14e245c66d6f",
+    "url": "/static/media/user-plus.5047d004.svg"
+  }
+]);
